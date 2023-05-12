1fe198a6b3f8afe6778bb7f5a0ac48d10cbae6c3
chiajeryoong <chiajy02@gmail.com>
2023-05-12 19:42:34 +0800

    api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import register, MyTokenObtainPairView, getAllCompany, getAllAnnouncements
+from .views import register, MyTokenObtainPairView, getAllCompany, getAnnouncements, getUserAnnouncements, getAnnouncement
+from . import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -10,9 +11,24 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('register', register, name='register'),
     path('login', MyTokenObtainPairView.as_view(), name='login'),
-    path('companys', getAllCompany, name='company'),
-    path('announcement', getAllAnnouncements, name='announcement'),
+    
+    path('company/<str:pk>', views.getCompany, name='getCompany'),
+    path('createCompany', views.createCompany, name='createCompany'),
+    path('updateCompany/<str:pk>', views.updateCompany, name='updateCompany'),
+    path('deleteCompany/<str:pk>', views.deleteCompany, name='deleteCompany'),
+
+    path('addCompanyEmployee', views.addCompanyEmployee, name='addCompanyEmployee'),
+    path('companyEmployee/<str:pk>', views.getCompanyEmployee, name='getCompanyEmployee'),
+    path('updateCompanyEmployee/<str:pk>', views.updateCompanyEmployee, name='updateCompanyEmployee'),
+    path('deleteCompanyEmployee/<str:pk>', views.deleteCompanyEmployee, name='deleteCompanyEmployee'),
+
+    path('announcements', getAnnouncements, name='announcements'),
+    path('announcementsuser/<str:companyId>/<str:userId>', getUserAnnouncements, name='announcementsByUser'),
+    path('announcement/<str:pk>', getAnnouncement, name='announcement'),
     
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    
+    
+    path('companys', getAllCompany, name='company'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+import jwt
 from rest_framework import status
 from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken
 from .serializers import UserSerializer, MyTokenObtainPairSerializer
 from .models import CustomOutstandingToken, CustomTokenUser , UserAccount
 from . import models
 from . import serializers
+from django.db.models import Q
 # from django.http import JsonResponse
 
 
@@ -42,16 +44,130 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def createCompany(request):
+    companyData = request.data
+    createdCompany = models.Company.objects.create(
+        name = companyData['name'],
+        email = companyData['email'],
+        phone = companyData['phone'],
+        address = companyData['address'],
+        company_desc = companyData['company_desc'],
+        company_benefits = companyData['company_benefits'],
+        boss_id = models.UserAccount.objects.get(id=companyData['boss_id']),
+        admin_id = models.UserAccount.objects.get(id=companyData['admin_id'])
+    )
+    serializer = serializers.CompanySerializer(createdCompany, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def getCompany(request, pk):
+    companyData = models.Company.objects.get(id=pk)
+    serializer = serializers.CompanySerializer(companyData, many=False)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateCompany(request, pk):
+    updatedCompany = request.data
+    originalCompany = models.Company.objects.get(id=pk)
+    serializer = serializers.CompanySerializer(instance=originalCompany, data=updatedCompany)
+    if(serializer.is_valid()):
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def deleteCompany(request, pk):
+    deletedCompany = models.Company.objects.get(id=pk)
+    deleted = deletedCompany.delete()
+    return Response(deleted)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def addCompanyEmployee(request):
+    data = request.data
+    employee = models.UserAccount.objects.get(id=data['user_id'])
+    company = models.Company.objects.get(id=data['company_id'])
+    addedEmployee = models.CompanyEmployee.objects.create(
+        role = data['role'],
+        position = data['position'],
+        salary = data['salary'],
+        company_id = company,
+        user_id = employee
+    )
+    serializer = serializers.CompanyEmployeeSerializer(addedEmployee, many=False)
+    return Response(serializer.data)
+
+@api_view(['get'])
+@permission_classes([AllowAny])
+def getCompanyEmployee(request, pk):
+    companyEmployee = models.CompanyEmployee.objects.filter(company_id=pk)
+    serializer = serializers.CompanyEmployeeSerializer(companyEmployee, many=True)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateCompanyEmployee(request, pk):
+    updatedCompanyEmployee = request.data
+    originalCompanyEmployee = models.CompanyEmployee.objects.get(id=pk)
+    serializer = serializers.CompanyEmployeeSerializer(instance=originalCompanyEmployee, data=updatedCompanyEmployee)
+    if(serializer.is_valid()):
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def deleteCompanyEmployee(request, pk):
+    deletedCompanyEmployee = models.CompanyEmployee.objects.get(id=pk)
+    delete = deletedCompanyEmployee.delete()
+    return Response(delete)
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def getAnnouncements(request):
+    announcementData = models.Announcement.objects.all()
+    serializer = serializers.AnnouncementSerializer(announcementData, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getUserAnnouncements(request, companyId, userId):
+    announcementData = models.Announcement.objects.filter(Q(company_id=companyId) & (Q(group__isnull=True) | Q(group__user_id__id=userId)))
+    serializer = serializers.AnnouncementSerializer(announcementData, many=True)
+    return Response(serializer.data)
+    '''
+    get announcement by group user id
+        group is ManyToManyField to table AnnouncementGroup, 
+        user_id is manyToManyField to table UserAccount,
+        id is field from table UserAccount
+        seperate double underscore
+    ManyToManyFieldInAnnouncementTABLE__ManyToManyFieldInAnnouncementGroupTABLE__fieldInUserAccountTABLE
+    announcementData = models.Announcement.objects.filter(group__user_id__id=2)
+    '''
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def getAnnouncement(request, pk):
+    announcementData = models.Announcement.objects.get(id=pk)
+    serializer = serializers.AnnouncementSerializer(announcementData, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def getIdFromToken(request):
+    # get token from header
+    token = request.META.get('HTTP_AUTHORIZATION', '').split(' ')[1]  
+    accessToken = AccessToken(token)
+    userId = accessToken.payload['user_id']
+    return Response(userId)
+
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def getAllCompany(request):
     companyData = models.Company.objects.all()
     serializer = serializers.CompanySerializer(companyData, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-@permission_classes([AllowAny])
-def getAllAnnouncements(request):
-    announcementData = models.Announcement.objects.all()
-    serializer = serializers.AnnouncementSerializer(announcementData, many=True)
     return Response(serializer.data)
